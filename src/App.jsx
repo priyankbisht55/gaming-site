d752b24bc964aac178d9eeab7c5896ed3723e978
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-12-17 12:53:15 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import Navbar from "./components/Navbar.jsx";
+import {useRef} from "react";
 
 const App = () => {
     return (
-        <main className="relative min-h-screen w-screen overflow-x-hidden">
+        <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
+            <Navbar />
             <Hero />
-
             <About />
         </main>
     )
