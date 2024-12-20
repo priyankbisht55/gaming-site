40e54b7cf230b0a460ff5b43fe35161c7c5708c6
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-12-20 14:57:48 +0530

    working on components

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Navbar from "./components/Navbar.jsx";
 import {useRef} from "react";
 import Features from "./components/Features.jsx";
 import Story from "./components/Story.jsx";
+import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
@@ -13,6 +15,8 @@ const App = () => {
             <About />
             <Features />
             <Story />
+            <Contact />
+            <Footer />
         </main>
     )
 }
