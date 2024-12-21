d0971ce18f2a2d777dab1d29a779f3eed591d43e
priyankbisht55 <135156326+priyankbisht55@users.noreply.github.com>
2024-12-21 17:49:46 +0530

    working on components

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,8 @@
-const ImageClipBox = ({ src, clipClass }) => (
+import Button from "./Button.jsx";
+
+const ImageClipBox = ({src, clipClass}) => (
     <div className={clipClass}>
-        <img src={src} />
+        <img src={src}/>
     </div>
 )
 
@@ -17,6 +19,26 @@ const Contact = () => {
                         clipClass="contact-clip-path-2 lg:translate-y-40 translate-y-60"
                         src="img/contact-2.webp"
                     />
+                </div>
+
+                <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-10 lg:top-20 lg:w-80">
+                    <ImageClipBox
+                        src="img/swordman-partial.webp"
+                        clipClass="absolute md:scale-125"
+                    />
+                    <ImageClipBox
+                        src="img/swordman.webp"
+                        clipClass="sword-man-clip-path md:scale-125"
+                    />
+                </div>
+
+                <div className="flex flex-col items-center text-center">
+                    <p className="font-general text-[10px] uppercase">Join Zentry</p>
+
+                    <p className="special-font mt-10 w-full font-zentry text-5xl leading-[0.9] md:text-[6rem]">
+                        Let's b<b>u</b>ild the <br /> new era of <br /> g<b>a</b>ming t<b>o</b>gether</p>
+
+                    <Button title="contact us" containerClass="mt-10 cursor-pointer"/>
                 </div>
             </div>
         </div>
